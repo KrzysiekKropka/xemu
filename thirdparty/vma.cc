5b2b18183b63a14297c6a81da14adf0a9fefc231
KrzysiekKropka <91200856+KrzysiekKropka@users.noreply.github.com>
2026-02-25 21:54:39 +0100

    Revert "thirdparty: silence GCC warnings in stb_image/vma wrappers"
    This reverts commit a52d5fc35d38b7055661ee5d7000626874881263.

--- thirdparty/vma.cc
+++ thirdparty/vma.cc
@@ -1,11 +1,3 @@
 #define VMA_IMPLEMENTATION
 #include <volk.h>
-
-#if defined(__GNUC__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-variable"
-#endif
 #include "vk_mem_alloc.h"
-#if defined(__GNUC__)
-#pragma GCC diagnostic pop
-#endif
--- ui/thirdparty/stb_image/stb_image_impl.c
+++ ui/thirdparty/stb_image/stb_image_impl.c
@@ -1,10 +1,2 @@
 #define STB_IMAGE_IMPLEMENTATION
-
-#if defined(__GNUC__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wmissing-prototypes"
-#endif
 #include "stb_image.h"
-#if defined(__GNUC__)
-#pragma GCC diagnostic pop
-#endif
