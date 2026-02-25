a52d5fc35d38b7055661ee5d7000626874881263
KrzysiekKropka <91200856+KrzysiekKropka@users.noreply.github.com>
2026-02-25 21:39:42 +0100

    thirdparty: silence GCC warnings in stb_image/vma wrappers

--- thirdparty/vma.cc
+++ thirdparty/vma.cc
@@ -1,3 +1,11 @@
 #define VMA_IMPLEMENTATION
 #include <volk.h>
+
+#if defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#endif
 #include "vk_mem_alloc.h"
+#if defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
